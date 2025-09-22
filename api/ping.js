ec1840ca082b713b461887d4f59ae0509b74d5f6
BaltaXYZ <baltafamiljen3@gmail.com>
2025-09-22 17:32:23 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,5 +1,8 @@
 // api/ping.js
-export const config = {}; // Node serverless (inte Edge)
-export default function handler(req, res) {
-  res.status(200).setHeader("content-type","text/plain; charset=utf-8").send("ok");
-}
+module.exports = (req, res) => {
+  res
+    .status(200)
+    .setHeader("content-type", "text/plain; charset=utf-8")
+    .send("ok");
+};
+
